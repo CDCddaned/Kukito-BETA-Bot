08de6e708c747948f4e148cbe78e50b3ab533d54
Glitch (hello-express) <none>
2020-07-16 15:23:20 +0000

    👩‍❤️‍💋‍👩🚤 Checkpoint ./commands/limpar.js:19165185/509

--- commands/limpar.js
+++ commands/limpar.js
@@ -5,6 +5,13 @@ exports.run = async (client, message, args) => {
 if (!message.member.hasPermission("MENAGE_MESSAGES"))
   return message.reply("Você não tem permissão pra usar esse comando");
 const deleteCount = parseInt(args[0], 10);
-if (!deleteCount )  
+if (!deleteCount || deleteCount <1 || deleteCount >100) 
+  return message.reply(":x: você digitou um número invalido escolha um número de 1 à 100");
+  
+const fetched = await message.channel.messages.fetch({ limit: deleteCount + 1});
+message.channel
+  .bulkDelete(fetched)
+  message.channel.send(`${args[0] as mensagens foram limpas com sucesso`)  
+  .catch(error)
   
 }

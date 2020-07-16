b8623450619b1fc3e83dabbeeab434ed05a50dc0
Glitch (hello-express) <none>
2020-07-16 15:11:17 +0000

    🚁👩‍❤️‍💋‍👩 Checkpoint ./commands/limpar.js:19165185/288

--- commands/limpar.js
+++ commands/limpar.js
@@ -1,1 +1,10 @@
 const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+  
+if (!message.member.hasPermission("MENAGE_MESSAGES"))
+  return message.reply("Você não tem permissão pra usar esse comando");
+const deleteCount = parseInt(args[0], 10);
+if (!deleteCount )  
+  
+}

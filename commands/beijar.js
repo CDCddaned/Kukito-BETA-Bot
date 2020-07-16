0a80192e8144730f60d392d81ab6013a110135ab
Glitch (hello-express) <none>
2020-07-16 18:48:43 +0000

    🛬🦀 Checkpoint ./commands/beijar.js:19165185/876

--- commands/beijar.js
+++ commands/beijar.js
@@ -3,8 +3,17 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
   
 var list = [
-  '',
-  '',
+  'https://pa1.narvii.com/5823/f10cce909b5bfa6f05f0af496558a16ed4840c06_hq.gif',
+  'https://68.media.tumblr.com/3d93be7699a2ba8b4bc13a29a37b84ad/tumblr_odt2geMkpc1t86l7wo1_500.gif',
+  'https://media.giphy.com/media/G3va31oEEnIkM/giphy.gif',
+  'https://pa1.narvii.com/5823/a3561ade23b7721461065cf6cc14fdfca2b0c340_hq.gif',
+  'https://media.giphy.com/media/KH1CTZtw1iP3W/giphy.gif',
+  'https://media.giphy.com/media/hnNyVPIXgLdle/giphy.gif',
+  'https://media1.tenor.com/images/bc5e143ab33084961904240f431ca0b1/tenor.gif?itemid=9838409',
+  'https://pa1.narvii.com/5823/88d38f7dbb1a17ae47c557dc40f823200583d376_hq.gif',
+  'https://media1.tenor.com/images/119b1e7802d1fb1039aec55f75e71cdc/tenor.gif?itemid=3531829',
+  'https://pa1.narvii.com/6277/db2c346e5c2bb105a2686968281d6ee206840019_hq.gif',
+  'https://media1.tenor.com/images/40711a5b00fcf9918ddef1aa483d993f/tenor.gif?itemid=11737410',
   ''
 ]  
   
